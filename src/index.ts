d6f1cbc440bab96a31eb75247a5522cf10899ffb
arcaniussainey <br.3cow@gmail.com>
2025-02-01 19:21:48 -0500

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 "use client"; // This marks the entire library as client-side, which is accurate because we will be pre-rendering to static client side elements herein
 
 export { default as Button } from "./testButton";
-export { default as switchTheme } from "./switchTheme"
+export { default as switchTheme } from "./switchTheme";
